9c294a5c819d05bfd3a5b4ad08eca3046fecffdb
Aryan Tamang <aryantmg628@gmail.com>
2024-08-11 07:15:04 -0400

    Feat : Top bar and intro card added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,21 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Container } from "@mui/material";
+import { Box } from "@mui/material";
+import { TopBar } from "./components/top-bar/top-bar";
+import theme from "./theme";
+import { ThemeProvider } from "@mui/material";
+import { IntroCard } from "./components/intro-card/intro-card";
 
 function App() {
-  return <Container>This is the container</Container>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Box>
+        <TopBar />
+        <IntroCard />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
