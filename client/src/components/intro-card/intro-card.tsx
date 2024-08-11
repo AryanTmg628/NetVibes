0d4f00f27c4c61108191bece97ac2fbdd604e6ad
Aryan Tamang <aryantmg628@gmail.com>
2024-08-11 13:14:41 -0400

    refactor : changed color of card

--- client/src/components/intro-card/intro-card.tsx
+++ client/src/components/intro-card/intro-card.tsx
@@ -11,7 +11,7 @@ export const IntroCard = () => {
   return (
     <Card
       sx={{
-        backgroundColor: "primary.main",
+        backgroundColor: "primary.dark",
         border: "none",
         display: "flex",
         p: "5rem 20rem",
