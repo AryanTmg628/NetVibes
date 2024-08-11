050baeee3904ca36c69990411f66712851929392
Aryan Tamang <aryantmg628@gmail.com>
2024-08-11 13:13:56 -0400

    Feat : responsive nav bar added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,12 +8,14 @@ import { TopBar } from "./components/top-bar/top-bar";
 import theme from "./theme";
 import { ThemeProvider } from "@mui/material";
 import { IntroCard } from "./components/intro-card/intro-card";
+import { ResponsiveAppBar } from "./components/responsive-app-bar/responsive-app-bar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Box>
         <TopBar />
+        <ResponsiveAppBar />
         <IntroCard />
       </Box>
     </ThemeProvider>
