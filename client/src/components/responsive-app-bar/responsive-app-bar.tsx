247d941e6dea9549b34c56ffe270a6d1b182888f
Aryan Tamang <aryantmg628@gmail.com>
2024-08-12 06:21:53 -0400

    refactor : refactor the code

--- client/src/components/responsive-app-bar/responsive-app-bar.tsx
+++ client/src/components/responsive-app-bar/responsive-app-bar.tsx
@@ -1,15 +1,18 @@
 import {
   AppBar,
-  Button,
   Menu,
   MenuItem,
   Toolbar,
   Typography,
-  Box,
+  Stack,
 } from "@mui/material";
 import { FC, useState } from "react";
 import Iconify from "../common/iconify/iconify";
 import menus from "/src/data/menus.json";
+import FlexBox from "../../utils/box/styled-box";
+import HoverTypography from "../../utils/typography/styled-typography";
+import { Logo } from "../common/logo/logo";
+import { MenuSubContentInterface, SingleMenuInterface } from "../../interfaces";
 
 export const ResponsiveAppBar: FC = () => {
   return (
@@ -18,10 +21,25 @@ export const ResponsiveAppBar: FC = () => {
       sx={{
         backgroundColor: "text.primary",
         boxShadow: "none",
+        px: "20rem",
       }}
     >
       <Toolbar disableGutters>
-        <FeatureMenu />
+        <FlexBox
+          gap="1rem"
+          justifyContent="space-between"
+          alignItems="center"
+          width="100%"
+        >
+          <Logo sx={{ flex: 1 }} />
+          {menus.menus.map((menu: SingleMenuInterface, index: number) => (
+            <SingleMenu
+              name={menu.name}
+              key={index}
+              subcontent={menu?.subcontent}
+            />
+          ))}
+        </FlexBox>
       </Toolbar>
     </AppBar>
   );
@@ -64,5 +82,76 @@ export const FeatureMenu = () => {
         ))}
       </Menu>
     </>
+  );
+};
+
+const SingleMenu: FC<SingleMenuInterface> = ({ name, subcontent }) => {
+  const [anchorELNav, setAnchorELNav] = useState<null | HTMLElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const openMenuItem = (event: React.MouseEvent<HTMLElement>) => {
+    console.log(event.currentTarget);
+    setAnchorELNav(event.currentTarget);
+    setIsMenuOpen(true);
+  };
+
+  const closeMenuItem = () => {
+    setAnchorELNav(null);
+    setIsMenuOpen(false);
+  };
+  const handleMouseLeave = () => {
+    setIsMenuOpen(false);
+  };
+  return (
+    <FlexBox
+      alignItems="center"
+      onClick={openMenuItem}
+      aria-controls="single-menu"
+    >
+      <HoverTypography variant="body2" color="text.black">
+        {name}
+      </HoverTypography>
+      <Iconify
+        icon="mdi-light:chevron-down"
+        color="text.black"
+        sx={{ "&:hover": { cursor: "pointer" } }}
+      />
+      <Menu
+        id="single-menu"
+        anchorEl={anchorELNav}
+        open={isMenuOpen}
+        onClose={closeMenuItem}
+        onMouseLeave={handleMouseLeave}
+        keepMounted
+      >
+        {subcontent?.map((menu: MenuSubContentInterface, index: number) => (
+          <MenuItem
+            key={index}
+            sx={{
+              color: "text.black",
+              p: "0.5rem 2rem",
+              display: "flex",
+              gap: "1rem",
+              alignItems: "center",
+              "&:hover": { cursor: "pointer" },
+            }}
+          >
+            {menu.icon && (
+              <Iconify
+                width={20}
+                icon={menu.icon}
+                color="primary.light"
+                sx={{ "&:hover": { cursor: "pointer" } }}
+              />
+            )}
+            <Stack spacing={0.3}>
+              <Typography variant="body2">{menu.name}</Typography>
+              <Typography variant="body1" color="custom.grey.200">
+                {menu?.description}
+              </Typography>
+            </Stack>
+          </MenuItem>
+        ))}
+      </Menu>
+    </FlexBox>
   );
 };
