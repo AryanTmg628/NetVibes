3280c64290558db93668bb32b73895a2bee85778
Aryan Tamang <aryantmg628@gmail.com>
2024-08-13 14:39:35 +0545

    feat : router added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Box } from "@mui/material";
-import { TopBar } from "./components/top-bar/top-bar";
 import theme from "./theme";
 import { ThemeProvider } from "@mui/material";
-import { IntroCard } from "./components/intro-card/intro-card";
-import { ResponsiveAppBar } from "./components/responsive-app-bar/responsive-app-bar";
+import { Router } from "./routes/sections";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Box>
-        <TopBar />
-        <ResponsiveAppBar />
-        <IntroCard />
-      </Box>
+      <Router />
     </ThemeProvider>
   );
 }
