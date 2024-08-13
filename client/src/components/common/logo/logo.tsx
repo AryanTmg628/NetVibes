f6f48bbf28d4d19db89516912afc4ecf2d916ae1
Aryan Tamang <aryantmg628@gmail.com>
2024-08-13 14:39:47 +0545

    refactor : refactor the code

--- client/src/components/common/logo/logo.tsx
+++ client/src/components/common/logo/logo.tsx
@@ -5,13 +5,19 @@ import company from "/src/data/company.json";
 
 interface LogoInterface {
   sx?: object;
+  iconColor?: string;
+  textColor?: string;
 }
 
-export const Logo: FC<LogoInterface> = ({ sx }) => {
+export const Logo: FC<LogoInterface> = ({ sx, iconColor, textColor }) => {
   return (
     <Stack sx={{ ...sx }} spacing={1} direction="row" alignItems="center">
-      <Iconify icon="mdi:server-security" width={30} color="text.black" />
-      <Typography variant="h5" color="text.black">
+      <Iconify
+        icon="mdi:server-security"
+        width={30}
+        color={iconColor ? iconColor : "text.black"}
+      />
+      <Typography variant="h5" color={textColor ? textColor : "text.black"}>
         {company.name}
       </Typography>
     </Stack>
