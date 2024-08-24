52a963db7f655c5b6ce994a0512149e7a1b39657
Aryan Tamang <aryantmg628@gmail.com>
2024-08-24 19:38:24 +0545

    Feat : faq added

--- client/src/pages/landing-page/landing-page.tsx
+++ client/src/pages/landing-page/landing-page.tsx
@@ -1,18 +1,20 @@
-import { Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import { TopBar } from "../../components/top-bar/top-bar";
 import { ResponsiveAppBar } from "../../components/responsive-app-bar/responsive-app-bar";
 import { IntroCard } from "../../components/intro-card/intro-card";
 import { FeaturesSection } from "../../modules/landing-page/features-section";
 import { CommunicateChannel } from "../../components/communicate-channel/communicate-channel";
+import { FAQ } from "../../components/faq/faq";
 
 export const LandingPage = () => {
   return (
-    <Box>
+    <Stack spacing={3}>
       <TopBar />
       <ResponsiveAppBar />
       <IntroCard />
       <FeaturesSection />
       <CommunicateChannel />
-    </Box>
+      <FAQ />
+    </Stack>
   );
 };
