8e7fe789d4c661a04cba690cc0483b20800caaa1
Aryan Tamang <aryantmg628@gmail.com>
2024-08-24 19:29:24 +0545

    Feat : Added faq component

---
+++ client/src/components/faq/faq.tsx
@@ -0,0 +1,28 @@
+import { Stack, Typography } from "@mui/material";
+import { ContentCollapse } from "../common/content-collapse/content-collapse";
+import FlexBox from "../../utils/box/styled-box";
+import faq from "../../data/faq.json";
+
+export const FAQ = () => {
+  return (
+    <FlexBox flexDirection="column" alignItems="center" paddingY={3}>
+      <Stack spacing={1} width="90%" maxWidth="700px">
+        <Typography variant="h5" color="primary.dark">
+          Frequently Asked Questions{" "}
+        </Typography>
+        <Typography variant="body1" color="custom.grey.200">
+          Here are some of the frequently asked questions and their answers.{" "}
+        </Typography>
+        <Stack spacing={4} paddingTop={3}>
+          {faq?.questions?.map((question, index) => (
+            <ContentCollapse
+              key={index}
+              title={question.title}
+              subContent={question.subContent}
+            />
+          ))}
+        </Stack>
+      </Stack>
+    </FlexBox>
+  );
+};
