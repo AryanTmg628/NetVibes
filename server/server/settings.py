dec3d808e1166e01ba4a55709a4cd749e0ce6a0a
Aryan Tamang <aryantmg628@gmail.com>
2024-09-01 09:33:48 +0545

    Feat : Added cors

--- server/server/settings.py
+++ server/server/settings.py
@@ -49,11 +49,13 @@ INSTALLED_APPS = [
     "domain",
     "rest_framework_swagger",
     "drf_yasg",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -140,6 +142,9 @@ TEMPLATES = [
         },
     }
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
