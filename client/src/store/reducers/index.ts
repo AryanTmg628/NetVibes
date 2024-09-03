dac449f5c3d608161fce0720ebb4e7d4212e9144
Aryan Tamang <aryantmg628@gmail.com>
2024-09-03 22:47:18 +0545

    Feat : authSlice and reducer added

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import domainReducer from "./domain/domainSlice";
+import { authReducer } from "./auth/auth-slice";
 
 export const RootReducer = combineReducers({
   // domain
   domain: domainReducer,
+  auth: authReducer,
 });
