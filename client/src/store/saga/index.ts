fe9b9cd86574942a0af729bb1e92fe47083a74ad
Aryan Tamang <aryantmg628@gmail.com>
2024-09-03 22:47:40 +0545

    Feat : authsaga added for side effects

--- client/src/store/saga/index.ts
+++ client/src/store/saga/index.ts
@@ -1,9 +1,12 @@
 import { takeLatest, all } from "redux-saga/effects";
 import { domainActions } from "../actions/domain/domainActions";
 import { fetchDomainDetailsSaga } from "./domain/fetch-domain-details-saga";
+import { authActions } from "../actions/auth/auth-actions";
+import { registerUserSaga } from "./auth/register-user-saga";
 
 export function* rootSaga() {
   yield all([
     takeLatest(domainActions.fetchDomainDetails, fetchDomainDetailsSaga),
+    takeLatest(authActions.registerUser, registerUserSaga),
   ]);
 }
