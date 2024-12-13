ad6d4db3bc861c9e57ff2ac0b0550d182c69707d
Aryan Tamang <aryantmg628@gmail.com>
2024-12-13 09:57:55 +0545

    feat : refactor the code

--- server/authentication/services.py
+++ server/authentication/services.py
@@ -61,6 +61,11 @@ class AuthenticationService:
                 "last_name": user.last_name,
                 "email": user.email,
                 "username": user.username,
+                "country" : user.country,
+                "city" : user.city,
+                "state" : user.state,
+                "street_address" : user.street_address,
+                "phone_number" : user.phone_number,
                 "iat": datetime.now().timestamp(),
             }
 
